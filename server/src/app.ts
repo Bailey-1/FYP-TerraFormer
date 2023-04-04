2b3a802ccb82ebf44892ba3e6a88470d03359f14
Bailey-1 <44928829+Bailey-1@users.noreply.github.com>
2023-04-04 22:41:41 +0100

    Small changes for screenshot

--- server/src/app.ts
+++ server/src/app.ts
@@ -58,15 +58,7 @@ app.post('/api/generateHcl', (req: Request, res: Response) => {
         }[];
         provider: {
             azurerm: {
-                features: [
-                    {
-                        // resource_group: [
-                        //     {
-                        //         prevent_deletion_if_contains_resources: true;
-                        //     },
-                        // ];
-                    },
-                ];
+                features: [object];
             };
         };
         resource: {
